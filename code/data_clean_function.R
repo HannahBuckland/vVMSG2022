18b3ff68f44bcd9b56328af05bcbdaeef550c98c
Hannah Buckland <hannah.buckland@bristol.ac.uk>
2022-01-11 12:40:27 +0000

    commenting code

--- code/data_clean_function.R
+++ code/data_clean_function.R
@@ -5,12 +5,12 @@
 
 data_clean <- function(x){
   
-  mean_TOTAL <- mean(x$TOTAL)
-  sd_TOTAL <- sd(x$TOTAL)
+  mean_TOTAL <- mean(x$TOTAL) # calculates the mean of the analytical totals
+  sd_TOTAL <- sd(x$TOTAL) # calculates the standard deviation of the analytical totlals
   
   probe_dat_clean <- x %>%
-    filter(TOTAL <= mean_TOTAL + 2*sd_TOTAL & TOTAL >= mean(TOTAL) - 2*sd(TOTAL)) %>%
-    filter(Al2O3 < 25) 
+    filter(TOTAL <= mean_TOTAL + 2*sd_TOTAL & TOTAL >= mean(TOTAL) - 2*sd(TOTAL)) %>% # excludes analyses with anomalous totals
+    filter(Al2O3 < 25) # removes analyses that you likely hit a crystal
   
-  return(probe_dat_clean)
+  return(probe_dat_clean) # returns the cleaned data frame
 }
